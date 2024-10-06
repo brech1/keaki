90d62d2657ab1430035e79789a292a7109ae464e
Vivek Bhupatiraju <vivekab@mit.edu>
2024-10-05 17:19:57 -0700

    fix broken test

--- tests/laconic_ot.rs
+++ tests/laconic_ot.rs
@@ -67,7 +67,6 @@ impl<E: Pairing> Receiver<E> {
             we,
             boolean_choices,
             commitment,
-            selection_polynomial,
             proofs, // Store the precomputed proofs
         }
     }
